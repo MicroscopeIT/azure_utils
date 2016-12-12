43f27d74da6487c337609b910c0bf9fc85f0dc69
Olek Cieślak <olek.cieslak@yahoo.com>
2016-12-12 15:30:19 +0000

    Azure Compute utils added

--- azure_utils.js
+++ azure_utils.js
@@ -2,7 +2,9 @@ const queues = require('./lib/sb/servicebus_queues')
 const queuesCommon = require('./lib/sb/servicebus_common')
 const blob = require('./lib/blob/blob_client')
 const permissions = require('./lib/blob/storage_permissions')
+const compute = require('./lib/compute/compute_client')
 
+module.exports.createComputeClient = compute.createComputeClient
 module.exports.createPermissionsClient = permissions.createPermissionsClient
 module.exports.createQueueServiceBus = queues.createQueueServiceBus
 module.exports.blobClient = blob
